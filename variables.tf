d59e8b37da237b46295500a94d327394d5a7d07d
Ravikumar Amireddy <ravi.amireddy@encora.com>
2024-09-30 21:19:31 +0530

    terraform module

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "ami_id" {
-  type        = string
-#   default     = "ami-09c813fb71547fc4f"
+  type = string
+  #   default     = "ami-09c813fb71547fc4f"
 }
 
 variable "instance_type" {
-  default     = "t3.micro"
-  type = string
+  default = "t3.micro"
+  type    = string
   validation {
-    condition     = contains(["t3.micro","t3.medium","t3.small"], var.instance_type)
+    condition     = contains(["t3.micro", "t3.medium", "t3.small"], var.instance_type)
     error_message = "Instance type can only be any one of t3.micro, t3.medium and t3.small"
   }
 }
